dfa46c5e1ca9662cffabd63e8a51fc7e847a37bd
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-04-07 19:57:46 +0000

    Add spinner animation

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,25 @@
-// Spinner element displayed when waiting for results
+import React from "react";
+import "./Spinner.css";
+
+/*
+    Spinner element displayed when waiting for results
+    From: http://cssload.net/en/spinners/
+    Author: https://codepen.io/Terramaster/
+*/
 export function Spinner() {
-  return (
-      <div className="spinner">
-        <div className="overlay">
-          <div className="loader">this is a spinner</div>
+    return (
+        <div id="spinner" class="osq-pane osq-pane-results">
+            <div className="overlay">
+                <div className="loader">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    );
 }
