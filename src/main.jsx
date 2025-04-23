fda18d977f21775d7e5f70c3b43befef2bef6e2c
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-04-23 09:58:51 -0700

    Add TODO for react-router

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// TODO: add react router when ready to pass queries to url
 // const router = createBrowserRouter([
 //   {
 //     path: "/",
