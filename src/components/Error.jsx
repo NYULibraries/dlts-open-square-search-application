725ec80e10fe6eafad1eb5a9536677900aae49e8
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-09-18 02:54:02 +0000

    Use better wording for errors

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import { PropTypes } from "prop-types";
 
 export function Error({ message }) {
-    return <div>Some error {message}</div>;
+    return (
+        <div>
+            {/* server call hangs */}
+            {/* server is off */}
+            {/* server responds with 404 or 500 */}
+            Apologies: technical difficulties. Please try again later {message}
+            Error contacting DLTS Viewer API: {message}
+            {/* no connection */}
+            Sorry, a server error has occurred. Please try your search again
+            later.
+        </div>
+    );
 }
 
 Error.propTypes = {
