0ed08fafb08819ebdb7c97387c933423a3317902
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-09-30 16:31:09 +0000

    Cleanup comments

--- src/components/ResultItem.jsx
+++ src/components/ResultItem.jsx
@@ -8,7 +8,6 @@ import "./ResultItem.css";
 
 // individual result item
 // no need for state, iterated by parent component
-// TODO: use es6 destructuring for default props
 export function ResultItem({
     contributors,
     date,
@@ -21,13 +20,11 @@ export function ResultItem({
 }) {
     return (
         <div
-            // container? or is article the container?
             id={identifier}
             key={identifier}
             name={title}
             className="book-summary-hold"
         >
-            {/* TODO: can we reduce containeritis and use article as the container */}
             <article className="book-summary container">
                 <div className="thumb" role="presentation">
                     <a
@@ -44,15 +41,12 @@ export function ResultItem({
                 </div>
                 <div className="meta">
                     <div className="book-title-group">
-                        {/* a tag not a react router item since we want to navigate to this page */}
                         <a href={`/books/${identifier}/`}>
                             <div
                                 className="book-title"
                                 // TODO: find a better solution that doesn't rely on dangerouslySetInnerHTML
                                 dangerouslySetInnerHTML={{ __html: title }}
                             ></div>
-
-                            {/* <div className="book-subtitle">{subtitle}</div> */}
                             <div
                                 className="book-subtitle"
                                 dangerouslySetInnerHTML={{ __html: subtitle }}
@@ -72,7 +66,6 @@ export function ResultItem({
                         <span>Published: </span>
                         <span>{date.substring(0, 4)}</span>
                     </div>
-                    {/* <div className="description meta">{description}</div> */}
                     <div
                         className="description meta"
                         dangerouslySetInnerHTML={{ __html: description }}
@@ -86,9 +79,6 @@ export function ResultItem({
 // TODO: proptypes deprecated in react 19, use typescript
 // https://react.dev/blog/2024/04/25/react-19-upgrade-guide#removed-proptypes-and-defaultprops
 ResultItem.propTypes = {
-    // contributors is a stringified json object that needs parsing
-    // contributors: PropTypes.array,
-    // skipping viewer api and going direct to solr
     contributors: PropTypes.string,
     date: PropTypes.string,
     identifier: PropTypes.string,
