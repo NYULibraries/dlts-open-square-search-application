079d837ad49e8e9deba1062c29b21f3c6a539a31
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-09-30 16:30:03 +0000

    Import styles from previous app

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./assets/sass/search-style.scss";
 
-// TODO: add react router when ready to pass queries to url
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     errorElement: <p>Error</p>,
-//   },
-// ]);
-
-// TODO: change root for app when ready to integrated with Opensquare
-ReactDOM.createRoot(document.getElementById("app")).render(
-    // <React.StrictMode>
-    //   <RouterProvider router={router} />
-    // </React.StrictMode>
-    <App />
-);
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
