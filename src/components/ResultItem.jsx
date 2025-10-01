5c1e6bc42942c60f84230a8a1389e381d3244916
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2025-10-01 15:54:18 +0000

    Remove CSS file import

--- src/components/ResultItem.jsx
+++ src/components/ResultItem.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 import { getThumbnailUrl } from "../utils/utils";
-import "./ResultItem.css";
 
 // individual result item
 // no need for state, iterated by parent component
